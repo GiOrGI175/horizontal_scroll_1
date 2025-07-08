e39d1012703b57ecf0f1919bdf435674a08665aa
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-07-08 14:53:24 +0400

    fix/build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import HorizontalScrollCarousel from '@/components/HorizontalScrollCarousel';
-import { motion, useTransform, useScroll } from 'framer-motion';
-import { useRef } from 'react';
 
 const Example = () => {
   return (
@@ -23,47 +21,3 @@ const Example = () => {
 };
 
 export default Example;
-
-type cardsT = {
-  url: string;
-  title: string;
-  id: number;
-};
-
-const cards: cardsT[] = [
-  {
-    url: '/imgs/abstract/1.jpg',
-    title: 'Title 1',
-    id: 1,
-  },
-  {
-    url: '/imgs/abstract/2.jpg',
-    title: 'Title 2',
-    id: 2,
-  },
-  {
-    url: '/imgs/abstract/3.jpg',
-    title: 'Title 3',
-    id: 3,
-  },
-  {
-    url: '/imgs/abstract/4.jpg',
-    title: 'Title 4',
-    id: 4,
-  },
-  {
-    url: '/imgs/abstract/5.jpg',
-    title: 'Title 5',
-    id: 5,
-  },
-  {
-    url: '/imgs/abstract/6.jpg',
-    title: 'Title 6',
-    id: 6,
-  },
-  {
-    url: '/imgs/abstract/7.jpg',
-    title: 'Title 7',
-    id: 7,
-  },
-];
